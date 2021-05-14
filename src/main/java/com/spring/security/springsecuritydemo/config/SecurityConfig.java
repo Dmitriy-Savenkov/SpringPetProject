7d33f6f535642ea9d82dd7db397f85d154170030
Dmitriy <dmitry.savenkov@bk.ru>
2021-05-14 14:32:46 +0300

    Login/Logout pages were changed to custom. Logout method is POST now

--- src/main/java/com/spring/security/springsecuritydemo/config/SecurityConfig.java
+++ src/main/java/com/spring/security/springsecuritydemo/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.spring.security.springsecuritydemo.config;
 import com.spring.security.springsecuritydemo.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config. annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.  annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -27,7 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest() // Каждый запрос
                 .authenticated()    // должен быть аутентифицирован (проверен, друг или враг)
                 .and()
-                .httpBasic();  // мы хотим использовать httpBasic Base64
+                .formLogin()  // мы хотим использовать кастомный метод авторизации
+                .loginPage("/auth/login").permitAll() // ссылка на кастомный метод авторизации
+                .defaultSuccessUrl("/auth/success") // ссылка на успешную авторизацию
+                .and()
+                .logout()   // Просим учесть в конфиге, что сы хотим натсроить LogOut
+                .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout", "POST"))   // LogOut обрабатываем AntPathRequestMatcher с методом Пост
+                .invalidateHttpSession(true)     // Инвалидируем (обновление элементов) сессию
+                .clearAuthentication(true)      // Очищаем аутентификацию (сущность, которая содержит инфу о юзере)
+                .deleteCookies("JSESSIONID")        // Удаляем кукки
+                .logoutSuccessUrl("/auth/login");       // Перенаправляем на успешную стр.
     }
 
     @Bean
